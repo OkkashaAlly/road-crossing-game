d2e5f73281da13f80475fc9ef8490d7e00e7e4f5
OkkashaAlly <steamnature006@gmail.com>
2023-12-09 13:34:32 +0300

    8. Rendering Sprites

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+import Phaser from "phaser";
 import "./style.css";
 
-import Phaser from "phaser";
+// creating scene
+const gameScene = new Phaser.Scene("Game");
 
-// creating scene 
-const gameScene = new Phaser.Scene('Game')
+// loading assets
+gameScene.preload = function () {
+  this.load.image("background", "/assets/background.png");
+  this.load.image("player", "/assets/player.png");
+};
 
-// setting game configuration 
+// called once after preload ends
+gameScene.create = function () {
+  // create bg sprite
+  // this.add.sprite(0, 0, "background").setOrigin(0, 0);
+  let bg = this.add.sprite(0, 0, "background");
+
+  const gameW = this.sys.game.config.width;
+  const gameH = this.sys.game.config.height;
+
+  bg.setPosition(gameW / 2, gameH / 2);
+};
+
+// setting game configuration
 const config = {
   type: Phaser.AUTO,
   width: 640,
   height: 360,
-  scene: gameScene
-}
+  scene: gameScene,
+};
 
-// creating the game 
-const game = new Phaser.Game(config)
+// creating the game
+const game = new Phaser.Game(config);
