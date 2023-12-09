0cfa121892738e9fa8cf2052b9fe8fe1d1cb23aa
OkkashaAlly <steamnature006@gmail.com>
2023-12-09 13:15:04 +0300

    Creating a new Phaser Game

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+import Phaser from "phaser";
+
+// creating scene 
+const gameScene = new Phaser.Scene('Game')
+
+// setting game configuration 
+const config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 360,
+  scene: gameScene
+}
+
+// creating the game 
+const game = new Phaser.Game(config)
