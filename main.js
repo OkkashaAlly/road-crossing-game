e2cc1a58ae98ffc839af0f0d4e406f62fd4bd6e0
OkkashaAlly <steamnature006@gmail.com>
2023-12-09 18:10:54 +0300

    18. Groups

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ import "./style.css";
 // creating scene
 const gameScene = new Phaser.Scene("Game");
 
+// ======================================================================
 // initial scene parameters
 gameScene.init = function () {
   this.playerSpeed = 3;
 
-  this.enemyMinSpeed = 2;
-  this.enemyMaxSpeed = 3;
+  this.enemyMinSpeed = 1;
+  this.enemyMaxSpeed = 2.5;
 
   // enemy boundary
   this.enemyMaxY = 280;
   this.enemyMinY = 80;
 };
 
+// ====================================================================
 // loading assets
 gameScene.preload = function () {
   this.load.image("background", "/assets/background.png");
@@ -24,6 +26,7 @@ gameScene.preload = function () {
   this.load.image("goal", "/assets/treasure.png");
 };
 
+// =====================================================================
 // called once after preload ends
 gameScene.create = function () {
   const gameH = this.sys.game.config.height;
@@ -37,22 +40,47 @@ gameScene.create = function () {
 
   this.player.setScale(0.5);
 
-  // create enemy
-  this.enemy = this.add.sprite(100, gameH / 2, "enemy").setScale(0.6);
-  this.enemy.flipX = true;
+  // create enemies(group)
+  this.enemies = this.add.group({
+    key: "enemy",
+    repeat: 5,
+    setXY: {
+      x: 90,
+      y: 100,
+      stepX: 80,
+      stepY: 20,
+    },
+  });
+  // adding sprite to a group
+  // this.enemies.add(this.enemy);
+  // console.log(this.enemies.getChildren());
 
-  // set enemy speed
-  const direction = Math.random() < 0.5 ? 1 : -1;
-  const speed =
-    this.enemyMinSpeed +
-    Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
-  this.enemy.speed = direction * speed;
-  console.log("🚀 ~ file: main.js:50 ~ this.enemy.speed:", this.enemy.speed)
+  const enemies = this.enemies.getChildren();
+
+  // scaling all enemies
+  Phaser.Actions.SetScale(enemies, 0.6);
+
+  // flipX and set speed
+  Phaser.Actions.Call(
+    enemies,
+    function (enemy) {
+      enemy.flipX = true;
+
+      // set enemy speed
+      const direction = Math.random() < 0.5 ? 1 : -1;
+      const speed =
+        this.enemyMinSpeed +
+        Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+      enemy.speed = direction * speed;
+    },
+    this
+  );
 
   // create goal
   this.goal = this.add.sprite(gameW - 80, gameH / 2, "goal").setScale(0.6);
 };
 
+// ====================================================================
 // called upto 60 times per second (frame rate)
 gameScene.update = function () {
   // check user input
@@ -69,19 +97,36 @@ gameScene.update = function () {
     this.scene.restart();
   }
 
-  // enemy movement
-  this.enemy.y += this.enemy.speed;
+  // get enemies
+  const enemies = this.enemies.getChildren();
 
-  // check we haven't passed min or max Y
-  const conditionUp = this.enemy.y <= this.enemyMinY;
-  const conditionDown = this.enemy.y >= this.enemyMaxY;
+  // loop over the enemies
+  for (let i = 0; i < enemies.length; i++) {
+    const enemy = enemies[i];
 
-  // if passed lower or higher limit, reverse the speed
-  if (conditionUp || conditionDown) {
-    this.enemy.speed *= -1;
+    // enemy movement
+    enemy.y += enemy.speed;
+
+    // check we haven't passed min or max Y
+    const conditionUp = enemy.y <= this.enemyMinY;
+    const conditionDown = enemy.y >= this.enemyMaxY;
+
+    // if passed lower or higher limit, reverse the speed
+    if (conditionUp || conditionDown) {
+      enemy.speed *= -1;
+    }
+
+    // check enemy overlap check
+    const enemyRect = enemy.getBounds();
+
+    if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)) {
+      console.log("Game over");
+      this.scene.restart();
+    }
   }
 };
 
+// ==================================================================
 // setting game configuration
 const config = {
   type: Phaser.AUTO,
