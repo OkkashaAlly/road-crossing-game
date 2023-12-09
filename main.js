59b715505415d74020aa864c680515396335d39f
OkkashaAlly <steamnature006@gmail.com>
2023-12-09 15:31:28 +0300

    14. Player and Goal

--- main.js
+++ main.js
@@ -4,58 +4,53 @@ import "./style.css";
 // creating scene
 const gameScene = new Phaser.Scene("Game");
 
+// initial scene parameters
+gameScene.init = function () {
+  this.playerSpeed = 2.5;
+};
+
 // loading assets
 gameScene.preload = function () {
   this.load.image("background", "/assets/background.png");
   this.load.image("player", "/assets/player.png");
   this.load.image("enemy", "/assets/dragon.png");
+  this.load.image("goal", "/assets/treasure.png");
 };
 
 // called once after preload ends
 gameScene.create = function () {
-  // create bg sprite
-  // this.add.sprite(0, 0, "background").setOrigin(0, 0);
-  const bg = this.add.sprite(0, 0, "background");
-
-  const gameW = this.sys.game.config.width;
   const gameH = this.sys.game.config.height;
+  const gameW = this.sys.game.config.width;
 
-  bg.setPosition(gameW / 2, gameH / 2);
+  // create bg sprite
+  this.add.sprite(0, 0, "background").setOrigin(0, 0);
 
   // create player
-  this.player = this.add.sprite(50, 180, "player");
-  // this.player.depth = 1 // layer position of sprite; default is 0
-  // this.player.x = 7; // alternative to setPosition()
+  this.player = this.add.sprite(40, gameH / 2, "player");
 
-  // this.player.setScale(2, 2);
-  // this.player.setScale(2);
+  this.player.setScale(0.5);
 
   // create enemy
   this.enemy = this.add.sprite(250, 180, "enemy");
-  this.enemy.scaleX = 2;
-  this.enemy.scaleY = 2;
 
-  // create second enemy
-  this.enemy2 = this.add.sprite(450, 180, "enemy");
-  this.enemy2.flipX = true;
-  this.enemy2.flipY = true;
-
-  // rotation
-  // this.enemy.setOrigin(0, 0);
-  // this.enemy.angle = 45
-  // this.enemy.setAngle(-45)
-  // this.enemy.rotation = Math.PI / 4
-  this.enemy.setRotation(Math.PI / 4);
+  // create goal
+  this.goal = this.add.sprite(gameW - 80, gameH / 2, "goal").setScale(0.6);
 };
 
 // called upto 60 times per second (frame rate)
 gameScene.update = function () {
-  // this.enemy.x += 1
-  // this.enemy.angle +=1
+  // check user input
+  if (this.input.activePointer.isDown) {
+    this.player.x += this.playerSpeed;
+  }
 
-  if (this.player.scaleX < 2) {
-    this.player.scaleX += 0.01;
-    this.player.scaleY += 0.01;
+  // treasure overlap check
+  const playerRect = this.player.getBounds();
+  const treasureRect = this.goal.getBounds();
+
+  if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
+    console.log("Goal");
+    this.scene.restart();
   }
 };
 
