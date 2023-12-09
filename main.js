33124bf2d631ba720b6540de4c1dce4fcdd17003
OkkashaAlly <steamnature006@gmail.com>
2023-12-09 11:38:35 +0300

    Project init

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-import Phaser from "phaser";
 import "./style.css";
-
-const config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
-  backgroundColor: 0x5f6e7a,
-};
-
-const game= new Phaser.Game(config)
-console.log("🚀 ~ file: main.js:12 ~ game:", game)
